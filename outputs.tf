08feca93d8ce00fc0eff33847f22a4148ae8f56b
Prathamesh Prakash Bhujade <129214303+prathamesh633@users.noreply.github.com>
2025-11-03 22:49:43 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,13 @@
+
 output "instance_id" {
   value = aws_instance.web.id
 }
 
 output "instance_public_ip" {
   value = aws_instance.web.public_ip
+}
+
+output "ssh_connection_command" {
+  description = "SSH connection command"
+  value       = "ssh -i ~/.ssh/office-key.pem ec2-user@${aws_instance.web.public_ip}"
 }
